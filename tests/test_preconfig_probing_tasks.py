055a9da368a22c21a9dca9ad05ce0b4a891babb6
felsiq <felipe.siqueira@usp.br>
2022-11-03 11:43:04 -0300

    tests: increase 'timeout_limit_seconds' 10 -> 60

--- tests/test_preconfig_probing_tasks.py
+++ tests/test_preconfig_probing_tasks.py
@@ -138,6 +138,7 @@ def test_preconfigured_probing_task(
         data_domain=data_domain,
         output_dir=os.path.join(os.path.dirname(__file__), "test_probing_datasets"),
         check_cached=False,
+        timeout_limit_seconds=60,
     )
 
     probing_factory = curiosidade.ProbingModelFactory(

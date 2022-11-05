2662debd340e1cf5d1c95fe6594d5cf9d1b06e32
felsiq <felipe.siqueira@usp.br>
2022-11-05 17:33:35 -0300

    [no ci] config: update version '0.5.2' -> '0.5.3'

--- curiosidade/__init__.py
+++ curiosidade/__init__.py
@@ -14,7 +14,7 @@ try:
     __version__ = _importlib_metadata.version(__name__)
 
 except _importlib_metadata.PackageNotFoundError:
-    __version__ = "0.5.2"
+    __version__ = "0.5.3"
 
 
 __all__ = [
